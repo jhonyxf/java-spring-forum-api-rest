08ad509aebc5d2a8eaf2dc2a5b5a2fd297324445
jhonyxf1 <jhonyxf1@gmail.com>
2019-11-05 10:34:19 -0300

    Implementação atualizar e excluir tópico

--- src/main/java/br/com/forum/controller/TopicosController.java
+++ src/main/java/br/com/forum/controller/TopicosController.java
@@ -3,13 +3,16 @@ package br.com.forum.controller;
 import java.net.URI;
 import java.util.List;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +20,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.forum.controller.dto.DetalhesDoTopicoDto;
 import br.com.forum.controller.dto.TopicoDto;
+import br.com.forum.controller.form.AtualizacaoTopicoForm;
 import br.com.forum.controller.form.TopicoForm;
 import br.com.forum.model.Topico;
 import br.com.forum.repository.CursoRepository;
@@ -57,6 +61,22 @@ public class TopicosController {
 	public DetalhesDoTopicoDto detalhar(@PathVariable Long id) {
 		Topico topico = topicoRepository.getOne(id);
 		return new DetalhesDoTopicoDto(topico);
+	}
+	
+	
+	@PutMapping("/{id}")
+	@Transactional
+	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form){
+		Topico topico = form.atualizar(id, topicoRepository);
+		
+		return ResponseEntity.ok(new TopicoDto(topico));
+	}
+	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity remover(@PathVariable Long id){
+		topicoRepository.deleteById(id);
+		return ResponseEntity.ok().build();
 	}
 	
 	
---
+++ src/main/java/br/com/forum/controller/form/AtualizacaoTopicoForm.java
@@ -0,0 +1,52 @@
+package br.com.forum.controller.form;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
+import br.com.forum.model.Topico;
+import br.com.forum.repository.TopicoRepository;
+
+public class AtualizacaoTopicoForm {
+	
+	@NotNull @NotEmpty @Length(min = 3)
+	private String titulo;
+	
+	@NotNull @NotEmpty
+	private String mensagem;
+	
+	
+		
+	
+	
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+
+	public Topico atualizar(Long id, TopicoRepository topicoRepository) {
+		Topico topico = topicoRepository.getOne(id);
+		topico.setTitulo(this.titulo);
+		topico.setMensagem(this.mensagem);
+		
+		return topico;
+	}
+	
+	
+	
+	
+	
+}
