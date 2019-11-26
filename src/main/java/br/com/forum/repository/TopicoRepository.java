6cfce8fd115b0d1a3ddfc29f30784e6adc00cce6
jhonyxf1 <jhonyxf1@gmail.com>
2019-11-26 10:35:25 -0300

    Implementando paginação

--- src/main/java/br/com/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/forum/repository/TopicoRepository.java
@@ -2,6 +2,8 @@ package br.com.forum.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.forum.model.Topico;
@@ -9,7 +11,7 @@ import br.com.forum.model.Topico;
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
 
 	//Buscando o atributo nome dentro da classe curso
-	List<Topico> findByCursoNome(String nomeCurso);
+	Page<Topico> findByCursoNome(String nomeCurso, Pageable paginacao);
 	
 	/*Caso queira fazer a consulta sem ser o do Spring
 	 * @Query("SELECT t FROM Topico t WHERE t.curso.nome = :nomeCurso")
