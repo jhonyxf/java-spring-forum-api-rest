9f02a9657b4f3d502e5e4b01a8d75dded22c2662
jhonyxf1 <jhonyxf1@gmail.com>
2019-11-27 12:30:50 -0300

    Autenticando usuário no Spring Security

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$aX1Pkvsn/fs7/9MwxDnNjeWz/Er/.k50OHBy2d.P5SaHO0fUbQba6');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
